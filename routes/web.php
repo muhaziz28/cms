d68f11b89968b68f425a93f62fe1ce9bbf75a2b3
Muhammad Haikal Aziz <muhammadhaikalaziz28@gmail.com>
2024-12-07 11:52:54 +0700

    feat: list article

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 
 Route::middleware('auth')->group(function () {
